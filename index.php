9f22b937194bfa7c07a0fba5f630001bacfad006
23Ankit11 <engg.ankit1103@gmail.com>
2018-02-06 19:43:15 +0530

    index addded

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
           <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
       <![endif]-->
       <link rel="icon" href="img/logo_square.ico" type="image/x-icon">
-                <script src="js/jquery.min.js"></script>
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
       
   </head>
 
