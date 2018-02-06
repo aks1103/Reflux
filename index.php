fe679468324d2e1791251744f62405ec66b062e1
23Ankit11 <engg.ankit1103@gmail.com>
2018-02-07 00:06:46 +0530

    debug Aks

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
     <!-- To prevent caching of the page -->
 
     <meta http-equiv="Pragma" content="no-cache">
@@ -35,7 +36,7 @@
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
         <!--[if lt IE 9]>
-          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js" charset="utf-8"></script>
           <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
       <![endif]-->
       <link rel="icon" href="img/logo_square.ico" type="image/x-icon">
@@ -886,30 +887,30 @@ EOL;
 
 
                 <!-- jQuery-->
-                <script src="js/jquery.min.js"></script>
-                <script src="js/jquery-migrate.min.js"></script>
-                <script src="bootstrap/js/bootstrap.min.js"></script>
-                <script src="js/jquery.easing.1.3.min.js" type="text/javascript"></script>
-                <script src="js/wow.min.js" type="text/javascript"></script>
-                <script src="js/jquery.sticky.js" type="text/javascript"></script>
-                <script src="js/waypoints.min.js" type="text/javascript"></script>
-                <script src="js/jquery.counterup.min.js" type="text/javascript"></script>
-                <script src="js/modernizr.custom.97074.js" type="text/javascript"></script>
-                <script src="js/parallax.min.js" type="text/javascript"></script>
-                <script src="js/jquery.flexslider-min.js" type="text/javascript"></script>
-                <script src="js/owl.carousel.min.js" type="text/javascript"></script>
-                <script src="js/one-page.js" type="text/javascript"></script>
+                <script src="js/jquery.min.js" charset="utf-8"></script>
+                <script src="js/jquery-migrate.min.js" charset="utf-8"></script>
+                <script src="bootstrap/js/bootstrap.min.js" charset="utf-8"></script>
+                <script src="js/jquery.easing.1.3.min.js" type="text/javascript" charset="utf-8"></script>
+                <script src="js/wow.min.js" type="text/javascript" charset="utf-8"></script>
+                <script src="js/jquery.sticky.js" type="text/javascript" charset="utf-8"></script>
+                <script src="js/waypoints.min.js" type="text/javascript" charset="utf-8"></script>
+                <script src="js/jquery.counterup.min.js" type="text/javascript" charset="utf-8"></script>
+                <script src="js/modernizr.custom.97074.js" type="text/javascript" charset="utf-8"></script>
+                <script src="js/parallax.min.js" type="text/javascript" charset="utf-8"></script>
+                <script src="js/jquery.flexslider-min.js" type="text/javascript" charset="utf-8"></script>
+                <script src="js/owl.carousel.min.js" type="text/javascript" charset="utf-8"></script>
+                <script src="js/one-page.js" type="text/javascript" charset="utf-8"></script>
                 <!--cube portfolio plugin-->
-                <script src="cubeportfolio/js/jquery.cubeportfolio.min.js" type="text/javascript"></script>
-                <script src="js/cube-portfolio.js" type="text/javascript"></script>
+                <script src="cubeportfolio/js/jquery.cubeportfolio.min.js" type="text/javascript charset="utf-8""></script>
+                <script src="js/cube-portfolio.js" type="text/javascript" charset="utf-8"></script>
                 <!--cantact form script-->
-                <script src="js/contact_me.js"  type="text/javascript"></script>
-                <script src="js/jqBootstrapValidation.js" type="text/javascript"></script>
+                <script src="js/contact_me.js"  type="text/javascript" charset="utf-8"></script>
+                <script src="js/jqBootstrapValidation.js" type="text/javascript" charset="utf-8"></script>
                 <script type="text/javascript" src="js/jasny-bootstrap.min.js"></script>
                 <script type="text/javascript" src="js/pace.min.js"></script>
                 <!--revolution slider plugins-->
-                <script src="rs-plugin/js/jquery.themepunch.tools.min.js" type="text/javascript"></script>
-                <script src="rs-plugin/js/jquery.themepunch.revolution.min.js" type="text/javascript"></script>
+                <script src="rs-plugin/js/jquery.themepunch.tools.min.js" type="text/javascript" charset="utf-8"></script>
+                <script src="rs-plugin/js/jquery.themepunch.revolution.min.js" type="text/java charset="utf-8"script"></script>
                 <script type="text/javascript" src="js/revolution-custom.js"></script>
                 <!--gmap js-->
                 <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=true&key=AIzaSyBqI2822ODn9hA_8Ovi0LiRhrAJ-sugw-g"></script>
